82ba224c2552109c635e377fd7989ca2cd085890
Jason Sprinkle <jason.sprinkle91@gmail.com>
2024-06-26 21:26:58 -0500

    add images; add mdx; add images to test blog post to see what layout would look like

--- README.md
+++ README.md
@@ -3,7 +3,9 @@ A blog for plants, food, and pets
 
 ## V1 release
 - [ ] Choose fonts, colors, logo
-- [ ] figure out blog layout will need to include things for: images, headers, font
+- [ ] figure out blog layout will need to include things for: images, headers
 - [ ] search blog posts
 - [ ] tags
 - [ ] need an about page
+- [ ] buy domain
+- [ ] netlify account
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, squooshImageService } from "astro/config";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  image: {
+    service: squooshImageService()
+  },
+  vite: {
+    assetsInclude: ["**/*.heic"]
+  },
+  integrations: [mdx()]
+});
